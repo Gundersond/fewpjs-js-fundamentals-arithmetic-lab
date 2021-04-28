c29f582d1542fb9b714c4c9ae8f77e51beaf60d4
Gundersond <dalton.gunderson@hotmail.com>
2021-04-27 20:38:54 -0500

    commit tree 0526875b3e0429efd44584c7bed9872819cb0f60

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-
-// Write your code here
+const num1 = 2;
+const num2 = 31;
+const multiply = (num1 * num2);
+const random = Math.round(Math.random() * 10) + 1;
+const num3 = 8;
+const num4 = 20;
+const mod = (num4 % num3);
+const max = Math.max(5, 10, 15, 20);
